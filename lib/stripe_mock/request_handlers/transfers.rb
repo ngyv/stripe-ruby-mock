09768f4a2ec03dc134fca945d88925622532931f
Yvonne Ng <ngyv.codes@gmail.com>
2021-12-09 14:24:03 -0500

    Add transfer reversals create and get methods
    Might have to reorder this so that it doesn't match all the reversal urls

--- lib/stripe_mock/request_handlers/transfers.rb
+++ lib/stripe_mock/request_handlers/transfers.rb
@@ -5,8 +5,11 @@ module StripeMock
       def Transfers.included(klass)
         klass.add_handler 'post /v1/transfers',             :new_transfer
         klass.add_handler 'get /v1/transfers',              :get_all_transfers
-        klass.add_handler 'get /v1/transfers/(.*)',         :get_transfer
         klass.add_handler 'post /v1/transfers/(.*)/cancel', :cancel_transfer
+        klass.add_handler 'post /v1/transfers/(.*)/reversals',     :new_transfer_reversal
+        klass.add_handler 'get /v1/transfers/(.*)/reversals',      :get_all_transfer_reversals
+        klass.add_handler 'get /v1/transfers/(.*)/reversals/(.*)', :get_transfer_reversal
+        klass.add_handler 'get /v1/transfers/(.*)',         :get_transfer
       end
 
       def get_all_transfers(route, method_url, params, headers)
@@ -70,6 +73,40 @@ module StripeMock
         assert_existence :transfer, $1, transfers[$1]
         t = transfers[$1] ||= Data.mock_transfer(:id => $1)
         t.merge!({:status => "canceled"})
+      end
+
+      def get_all_transfer_reversals(route, method_url, params, headers)
+        extra_params = params.keys - [:ending_before, :limit, :starting_after]
+        unless extra_params.empty?
+          raise Stripe::InvalidRequestError.new("Received unknown parameter: #{extra_params[0]}", extra_params[0].to_s, http_status: 400)
+        end
+ 
+        if params[:limit]
+          transfer_reversals = transfer_reversals.first([params[:limit], transfer_reversals.size].min)
+        end
+ 
+        Data.mock_list_object(transfer_reversals, params)
+      end
+ 
+      def new_transfer_reversal(route, method_url, params, headers)
+        route =~ method_url
+        id = new_id('trr')
+  
+        unless params[:amount].is_a?(Integer) || (params[:amount].is_a?(String) && /^\d+$/.match(params[:amount]))
+          raise Stripe::InvalidRequestError.new("Invalid integer: #{params[:amount]}", 'amount', http_status: 400)
+        end
+ 
+        transfer = assert_existence :transfer, $1, transfers[$1]
+        transfer_reversal = Data.mock_transfer_reversal(params.merge :id => id)
+        transfer[:reversals][:data] << transfer_reversal
+        transfer[:reversals][:total_count] = transfer[:reversals][:data].length
+        transfer_reversal
+      end
+ 
+      def get_transfer_reversal(route, method_url, params, headers)
+        route =~ method_url
+        transfer = assert_existence :transfer, $1, transfers[$1]
+        transfer_reversal = transfer[:reversals][:data].find{|trr| trr[:id] == $2 }
       end
     end
   end
